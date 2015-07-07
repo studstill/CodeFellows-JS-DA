36ee716ed53ac1e247a8293c3d49a6584aed86a0
Jason Studstill <standstill64@gmail.com>
2015-07-06 19:35:40 -0700

    Implement test of greet function in greet.js

--- simple-test-and-modular-patterns/test/greet-test.js
+++ simple-test-and-modular-patterns/test/greet-test.js
@@ -1,2 +1,8 @@
 var greet = require('../greet');
 var expect = require('chai').expect;
+
+describe('greet.js', function() {
+  it('will say "hello Jason" when passed Jason', function() {
+    expect(greet.greet('Jason')).to.eql('hello Jason');
+  });
+});
