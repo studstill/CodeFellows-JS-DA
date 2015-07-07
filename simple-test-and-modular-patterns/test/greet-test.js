ceb7a992e882aebdecba7246aa4f9bad2dcbdc32
Jason Studstill <standstill64@gmail.com>
2015-07-06 19:29:05 -0700

    Create greet-test.js

--- simple-test-and-modular-patterns/test/greet-test.js
+++ simple-test-and-modular-patterns/test/greet-test.js
@@ -1,8 +1,2 @@
 var greet = require('../greet');
 var expect = require('chai').expect;
-
-describe('greet.js', function() {
-  it('will say "hello Jason" when passed Jason', function() {
-    expect(greet.greet('Jason')).to.eql('hello Jason');
-  });
-});
