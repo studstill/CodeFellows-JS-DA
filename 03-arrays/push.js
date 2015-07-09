1185724d2e142b217adfc9769b20dc05c3b3e8db
Jason Studstill <standstill64@gmail.com>
2015-07-09 11:02:43 -0700

    Implement push(), with the intention of adding it to the Array.prototype

--- 03-arrays/push.js
+++ 03-arrays/push.js
@@ -1,2 +1,18 @@
 // The push() method adds one or more elements to the end of an array and
 // returns the new length of the array.
+
+module.exports = push;
+
+function push(element) {
+  this[this.length] = element;
+  return this.length;
+}
+
+// Array.prototype.push = function(element) {
+//   this[array.length] = element;
+//   return array.length;
+// };
+
+function List() {};
+
+List.prototype.run = function(speed) { console.log()};
