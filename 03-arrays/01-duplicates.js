145879fcb21d446e990ef805f75ec68120104712
Jason Studstill <standstill64@gmail.com>
2015-07-08 20:10:27 -0700

    Implement fastest solution that I know

--- 03-arrays/01-duplicates.js
+++ 03-arrays/01-duplicates.js
@@ -1,3 +1,20 @@
  // Given an array, check for duplicates.
  // Return true if duplicates, false if no duplicates
 
+function duplicates(array) {
+  var hash = {};
+  for (var i = 0; i < array.length; i++) {
+    if (hash[array[i]] === 1)
+      return true;
+    else
+      hash[array[i]] = 1;
+  }
+  console.log(hash);
+  return false;
+}
+
+// duplicates([1,2,2,3]);
+// >true
+
+// duplicates([1,2,3]);
+// >false
