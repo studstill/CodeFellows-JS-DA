5a0e5ddd2993fd3f9518fb4c8adcef963a63e94c
Jason Studstill <standstill64@gmail.com>
2015-07-09 12:38:43 -0700

    Implement my solution to shift to be added to Array.prototype

--- 03-arrays/shift.js
+++ 03-arrays/shift.js
@@ -1,3 +1,15 @@
 // The shift() method removes the first element from an array and
 // returns that element. This method changes the length of the array.
 
+module.exports = shift;
+
+function shift() {
+  var firstElement = this[0];
+  var myArr = [];
+  for (var i = 0; i < this.length - 1; i++) {
+    this[i] = this[i + 1];
+  }
+  this.length--;
+  return firstElement;
+}
+
