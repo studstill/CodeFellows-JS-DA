c8ad442eb0e9577cee6f6e562c765dc537fa039a
Jason Studstill <standstill64@gmail.com>
2015-07-08 21:44:34 -0700

    Refactor code using imported helper function

--- 03-arrays/02-most-repeated-charAt0.js
+++ 03-arrays/02-most-repeated-charAt0.js
@@ -1,2 +1,18 @@
 // Given an array of strings, return the character that
 // appears most often at the beginning of each string
+
+var highestValueKey = require('./helpers.js').highestValueKey;
+module.exports = mostRepeatedChar;
+
+function mostRepeatedChar(array) {
+  var hash = {};
+  for (var i = 0; i < array.length; i++) {
+    var firstChar = array[i].charAt(0);
+    if (firstChar in hash)
+      hash[firstChar]++;
+    else
+      hash[firstChar] = 1;
+  }
+  return highestValueKey(hash);
+}
+
