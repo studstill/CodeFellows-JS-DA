9d560e951883f9407a899081f9e69f2ed8279ff0
Jason Studstill <standstill64@gmail.com>
2015-07-09 23:52:15 -0700

    Open the bmp file with fs and convert the buffer to a javascript object

--- 04-bitmap/bitmap.js
+++ 04-bitmap/bitmap.js
@@ -6,9 +6,30 @@
 // sure to run all your code through jshint and jscs. The process will look
 // something like this:
 
+var fs = require('fs');
+
+var EventEmitter = require('events').EventEmitter;
+var ee = new EventEmitter();
+
 // 1. open file using fs and read it into a buffer
 
+ee.on('readFile', function() {
+  fs.readFile('palette-bitmap.bmp', function(err, data) {
+    if (err) throw err;
+    ee.emit('convertFile', data);
+    console.log(data);
+  });
+});
+
+ee.emit('readFile');
+
 // 2. convert buffer into a Javascript Object
+
+ee.on('convertFile', function(buf) {
+  var json = JSON.stringify(buf);
+
+  console.log(json);
+});
 
 // 3. Run a transform on that Javascript Object.
 
