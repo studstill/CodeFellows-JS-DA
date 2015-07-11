e72379aa975aa6739c4ae1b866086193b1e4390e
Jason Studstill <standstill64@gmail.com>
2015-07-11 09:55:56 -0700

    WIP - broken code

--- 04-bitmap/bitmap.js
+++ 04-bitmap/bitmap.js
@@ -7,33 +7,54 @@
 // something like this:
 
 var fs = require('fs');
-
 var EventEmitter = require('events').EventEmitter;
 var ee = new EventEmitter();
 
 // 1. open file using fs and read it into a buffer
 
 ee.on('readFile', function() {
-  fs.readFile('palette-bitmap.bmp', function(err, data) {
+  fs.readFile('non-palette-bitmap.bmp', function(err, data) {
     if (err) throw err;
+    var fileType = data.toString('ascii', 0, 2);
+    console.log(fileType);
+    console.log(data.readUInt32LE(58 + 40));
     ee.emit('convertFile', data);
-    console.log(data);
   });
 });
 
 ee.emit('readFile');
 
-// 2. convert buffer into a Javascript Object
+// 2. convert buffer into a Javascript Object - TODO
 
-ee.on('convertFile', function(buf) {
-  var json = JSON.stringify(buf);
+ee.on('convertFile', function(data) {
 
-  console.log(json);
+  // var json = JSON.stringify(data);
+  // var parsed = JSON.parse(json);
+  // var headers = parsed.data.splice(0, 54);
+  // var imageData = parsed.data.splice(53);
+  // ee.emit('transform', headers, imageData);
+
+
+  //console.log(headers+ " MORE STUFF COMING!\n");
+  // console.log('THIS BEGINS THE IMAGE DATA\n' + imageData);
 });
 
-// 3. Run a transform on that Javascript Object.
+// 3. Run a transform on that Javascript Object. - TODO
 
-// 4. Turn the transformed object back into a buffer.
+ee.on('transform', function(headers, imageData) {
+  var headers = headers;
+  var reversedImage = imageData.reverse();
+
+  // console.log(reversedImage);
+  ee.emit('convertToBuffer', headers, reversedImage);
+});
+
+// 4. Turn the transformed object back into a buffer. - TODO
+
+ee.on('convertToBuffer', function(headers, reversedImage) {
+  // Convert data back to buffer
+  console.log("I made it!");
+});
 
 // 5. Write that buffer to a new file.
 
