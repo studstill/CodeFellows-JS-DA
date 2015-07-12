5b40c2d3b457780ae12b8e0219f7237fadf490ca
Jason Studstill <standstill64@gmail.com>
2015-07-11 20:51:52 -0700

    Implement my solution for unique

--- 03-arrays/unique.js
+++ 03-arrays/unique.js
@@ -1,3 +1,18 @@
 // unique: implement a function called unique. It takes an array and
 // returns a copy of the array with all the duplicates removed.
 
+function unique(array) {
+  array = array.sort();
+  var newArray = [];
+  newArray.push(array[0]);
+  for (var i = 0, j = 1; j < array.length; j++) {
+    if (newArray[i] !== array[j]) {
+      newArray.push(array[j]);
+      i++;
+    }
+  }
+  return newArray;
+}
+
+module.exports = unique;
+
