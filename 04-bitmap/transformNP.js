5f769e9c0a13c817ad6d18c20038fb7c0a08641f
Jason Studstill <standstill64@gmail.com>
2015-07-13 09:27:07 -0700

    Final version of transformNP.js

--- 04-bitmap/transformNP.js
+++ 04-bitmap/transformNP.js
@@ -29,74 +29,79 @@ if (!cliArg || !(cliArg in options)) {
   throw err;
 }
 
-/************************************************
-/ 1. open file using fs and read it into a buffer
-/************************************************/
+function transformNP(cliArg, options) {
 
-ee.on('readFile', function() {
-  fs.readFile('lib/non-palette-bitmap.bmp', function(err, data) {
-    if (err) throw err;
-    ee.emit('convertFile', data);
+  /************************************************
+  / 1. open file using fs and read it into a buffer
+  /************************************************/
+
+  ee.on('readFile', function() {
+    fs.readFile('lib/non-palette-bitmap.bmp', function(err, data) {
+      if (err) throw err;
+      ee.emit('convertFile', data);
+    });
   });
-});
 
-ee.emit('readFile');
+  ee.emit('readFile');
 
-/************************************************
-/ 2. convert buffer into a Javascript Object
-/************************************************/
+  /************************************************
+  / 2. convert buffer into a Javascript Object
+  /************************************************/
 
-ee.on('convertFile', function(data) {
-  var imageBuf = data.slice(54);
-  var json = JSON.stringify(imageBuf);
-  var parsed = JSON.parse(json);
-  var imageDataArray = parsed.data;
-  ee.emit('transform', data, imageBuf, imageDataArray);
+  ee.on('convertFile', function(data) {
+    var imageBuf = data.slice(54);
+    var json = JSON.stringify(imageBuf);
+    var parsed = JSON.parse(json);
+    var imageDataArray = parsed.data;
+    ee.emit('transform', data, imageBuf, imageDataArray);
 
-});
-
-/************************************************
-/ 3. Run a transform on that Javascript Object
-/************************************************/
-
-ee.on('transform', function(data, imageBuf, imageDataArray) {
-
-  // Pull in the command line arguement to choose which function to run
-  var transformedArray = options[cliArg](imageDataArray, data);
-
-  ee.emit('convertToBuffer', data, imageBuf, transformedArray);
-});
-
-/************************************************
-/ 4. Turn the transformed object back into a buffer
-/************************************************/
-
-ee.on('convertToBuffer', function(data, imageBuf, transformedArray) {
-
-  // Sanity Check
-  if (imageBuf.length !== transformedArray.length)
-    console.error('There was an error during transformation');
-
-  // Write the transformed image back to the buffer
-  for (var i = 0; i < imageBuf.length; i++) {
-    var currentBufIndex = imageBuf.slice(i, i + 1);
-    currentBufIndex.writeUInt8(transformedArray[i]);
-  }
-
-  ee.emit('writeFile', data, imageBuf);
-});
-
-/************************************************
-/ 5. Write that buffer to a new file.
-/************************************************/
-
-ee.on('writeFile', function(data) {
-  fs.writeFile('lib/new-non-palette2.bmp', data, function(err) {
-    if (err) throw err;
   });
-  console.log('Image Transformation Using ' + cliArg.toUpperCase() +
-              ' Complete');
-});
 
+  /************************************************
+  / 3. Run a transform on that Javascript Object
+  /************************************************/
 
+  ee.on('transform', function(data, imageBuf, imageDataArray) {
+
+    // Pull in the command line arguement to choose which function to run
+    var transformedArray = options[cliArg](imageDataArray, data);
+
+    ee.emit('convertToBuffer', data, imageBuf, transformedArray);
+  });
+
+  /************************************************
+  / 4. Turn the transformed object back into a buffer
+  /************************************************/
+
+  ee.on('convertToBuffer', function(data, imageBuf, transformedArray) {
+
+    // Sanity Check
+    if (imageBuf.length !== transformedArray.length)
+      console.error('There was an error during transformation');
+
+    // Write the transformed image back to the buffer
+    for (var i = 0; i < imageBuf.length; i++) {
+      var currentBufIndex = imageBuf.slice(i, i + 1);
+      currentBufIndex.writeUInt8(transformedArray[i]);
+    }
+
+    ee.emit('writeFile', data, imageBuf);
+  });
+
+  /************************************************
+  / 5. Write that buffer to a new file.
+  /************************************************/
+
+  ee.on('writeFile', function(data) {
+    fs.writeFile('lib/new-non-palette2.bmp', data, function(err) {
+      if (err) throw err;
+    });
+    console.log('Image Transformation Using ' + cliArg.toUpperCase() +
+                ' Complete');
+  });
+}
+
+transformNP(cliArg, options);
+
+exports.transformNP = transformNP;
 
