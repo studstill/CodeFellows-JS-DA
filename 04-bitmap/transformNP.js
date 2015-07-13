03be56a401e2fbae4b9d62a0e864b4d6c76c35ea
Jason Studstill <standstill64@gmail.com>
2015-07-12 17:00:43 -0700

    Final Implementation of Solutions

--- 04-bitmap/transformNP.js
+++ 04-bitmap/transformNP.js
@@ -9,90 +9,94 @@
 var fs = require('fs');
 var EventEmitter = require('events').EventEmitter;
 var ee = new EventEmitter();
-var invert = require('./helpers').invertImageColors;
-var grayscale = require('./helpers').grayscaleColors;
-var flip = require('./helpers').flipImageNP;
-var brightness = require('./helpers').brightness;
+var invert = require('./helpers/helpers').invertImageColors;
+var grayscale = require('./helpers/helpers').grayscaleColors;
+var flip = require('./helpers/helpers').flipImageNP;
+var brightness = require('./helpers/helpers').brightness;
 var cliArg = process.argv[2];
 
+// Define allowable command line arguments
 var options = {
   'invert': invert,
   'grayscale': grayscale,
   'flip': flip
 };
 
+// Ensure proper usage of the program
 if (!cliArg || !(cliArg in options)) {
   err = 'Usage: node transformNP.js option\n' +
         'options: invert, grayscale, flip\n';
   throw err;
 }
 
-// 1. open file using fs and read it into a buffer
+/************************************************
+/ 1. open file using fs and read it into a buffer
+/************************************************/
 
 ee.on('readFile', function() {
   fs.readFile('lib/non-palette-bitmap.bmp', function(err, data) {
     if (err) throw err;
-
-    console.log(data.toString('ascii', 0, 2) + ': File Type'); // BM
-    console.log(data.readUInt32LE(2) + ': File Size'); // 30054
-    console.log(data.readUInt32LE(10) + ': Offset before which image data begins'); // 54
-    console.log(data.readUInt32LE(14) + ': Size of the header'); // 40
-    console.log(data.readUInt32LE(18) + ': BMP width in pixels');  // 100
-    console.log(data.readUInt32LE(22) + ': BMP height in pixels');  // 100
-    console.log(data.readUInt32LE(28) + ': Number of bits per pixel'); // 24
-    console.log(data.readUInt32LE(30) + ': Compression used');  // 0 (none)
-    console.log(data.readUInt32LE(34) + ': (Raw) Image size');  // 30000
-    console.log(data.readUInt32LE(46) + ': Number of colors in the palette');  // 0
-
     ee.emit('convertFile', data);
   });
 });
 
 ee.emit('readFile');
 
-// 2. convert buffer into a Javascript Object
+/************************************************
+/ 2. convert buffer into a Javascript Object
+/************************************************/
 
 ee.on('convertFile', function(data) {
   var imageBuf = data.slice(54);
   var json = JSON.stringify(imageBuf);
   var parsed = JSON.parse(json);
   var imageDataArray = parsed.data;
-
   ee.emit('transform', data, imageBuf, imageDataArray);
 
 });
 
-// 3. Run a transform on that Javascript Object. - TODO
+/************************************************
+/ 3. Run a transform on that Javascript Object
+/************************************************/
 
 ee.on('transform', function(data, imageBuf, imageDataArray) {
 
+  // Pull in the command line arguement to choose which function to run
   var transformedArray = options[cliArg](imageDataArray, data);
 
   ee.emit('convertToBuffer', data, imageBuf, transformedArray);
 });
 
-// 4. Turn the transformed object back into a buffer. - TODO
+/************************************************
+/ 4. Turn the transformed object back into a buffer
+/************************************************/
 
 ee.on('convertToBuffer', function(data, imageBuf, transformedArray) {
 
   // Sanity Check
   if (imageBuf.length !== transformedArray.length)
-    console.log("There was an error during transformation");
+    console.error('There was an error during transformation');
 
   // Write the transformed image back to the buffer
   for (var i = 0; i < imageBuf.length; i++) {
-    var currentBufIndex = imageBuf.slice(i, i + 1)
+    var currentBufIndex = imageBuf.slice(i, i + 1);
     currentBufIndex.writeUInt8(transformedArray[i]);
   }
 
-  ee.emit('writeFile', data, imageBuf)
+  ee.emit('writeFile', data, imageBuf);
 });
 
-// 5. Write that buffer to a new file.
+/************************************************
+/ 5. Write that buffer to a new file.
+/************************************************/
 
 ee.on('writeFile', function(data) {
   fs.writeFile('lib/new-non-palette2.bmp', data, function(err) {
     if (err) throw err;
   });
+  console.log('Image Transformation Using ' + cliArg.toUpperCase() +
+              ' Complete');
 });
+
+
 
